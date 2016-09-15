4f2a322b5ac399bee7c96d79a9df5960140be27b
Swornim Srestaa <srestaswrnm@gmail.com>
2016-09-15 15:08:46 +0545

    Static functio post added

--- libs/route.php
+++ libs/route.php
@@ -1,12 +1,38 @@
 <?php
 namespace App;
+  /*
+
+    Class Route is under namespace App. So before every file you need to enter
+        use App/Route
+    to use the class route independently. Route class takes three parameters while
+    making an object and five vairables are assinged in this class as private
+    variables. Three paramenters are $url, $handler and $method. $url is simply
+    the url of caught by bootstrap/app.php file.  $handler is the second parameter
+    of the get,post function and the last parameter $method is the http method.
+
+  */
 
 class Route{
+  //different private variables are declared
+
   private $controller,$function,$name,$url,$method;
 
+  //Contstructor taking three parameters.
   public function __construct($url,$handler,$method){
+
+    //Assiging the method and url to variables of class
+
     $this->method = $method;
     $this->url = $url;
+
+    /*
+
+      Checking if the $handler variable is empty if not then exploding it taking
+      @ and the separation element and taking first element of array as controller
+      and second as function.
+
+    */
+
     if(!empty($handler)){
 
       $handlerArray = explode('@',$handler);
@@ -16,9 +42,32 @@ class Route{
 
   }
 
+  /*
+
+    Get is a static function. It takes two arguments or parameters. One is url
+    and another one is controllers name. It simply return a route object. This
+    function is used in routes.php file to define the routes to different urls.
+
+  */
+
   public static function get($url,$controller){
 
     return new Route($url,$controller,'GET');
+
+  }
+
+  /*
+
+    Post is another static function that takes two arguments like get. It is very
+    similar to get you can check the get function for more information. It handles
+    the GET http request from the client. It also return a route object but the
+    third parameter is post.
+
+  */
+
+  public static function post($url,$controller){
+
+    return new Route($url,$controller,'POST');
 
   }
 
